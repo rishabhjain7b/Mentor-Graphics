6ffdd0a257a49e6a6082ea2b5b04828b48cf3d6c
The Lunatic diode <rishabhjain7b@gmail.com>
2017-07-02 18:54:34 +0530

    Delete avg.pl

--- avg.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-print("enter 5 numbers\n");
-$a=<STDIN>;
-$b=<STDIN>;
-$c=<STDIN>;
-$d=<STDIN>;
-$e=<STDIN>;
-print("sum is: ", $a+$b+$c+$d+$e,"\nAverage is: ",($a+$b+$c+$d+$e)/5,"\n"); 
